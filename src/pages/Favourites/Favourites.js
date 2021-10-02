e0bbccac7d833082a4405ae80ff23ec536d41f92
Ambika4 <kambika1309@gmail.com>
2021-10-02 21:40:30 +0530

    issue fixing

--- src/pages/Favourites/Favourites.js
+++ src/pages/Favourites/Favourites.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Table from "../../components/Table/Table";
+//import Table from "../../components/Table/Table";
 import "./Favourites.css";
 import { useHistory } from "react-router";
 
